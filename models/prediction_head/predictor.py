a346f821528ed727f8daff7567b26543d935356b
3rdCore <tom.marty@mila.quebec>
2024-05-23 15:17:06 -0400

    added MetaLinearPredictor

--- models/prediction_head/predictor.py
+++ models/prediction_head/predictor.py
@@ -20,7 +20,7 @@ class Predictor(ABC, nn.Module):
         self.y_dim = y_dim
 
     @abstractmethod
-    def forward(self, x: dict[str, Tensor], z: dict[str, Tensor]) -> dict[str, Tensor]:
+    def forward(self, x: dict[str, Tensor], z: dict[str, Tensor]) -> dict[tuple[str, str], Tensor]:
         """Predict given inputs x and model parameters z.
 
         Args:
@@ -28,7 +28,7 @@ class Predictor(ABC, nn.Module):
             z (dict[str, Tensor]): Aggregated context information, each with shape (samples, tasks, *).
 
         Returns:
-            dict[str, Tensor]: Predicted outputs, each with shape (samples, tasks, *).
+            dict[tuple[str, str], Tensor]: Predicted values for each pair (input_name, output_name), each with shape (samples, tasks, *).
         """
         pass
 
@@ -80,7 +80,7 @@ class VanillaPredictor(Predictor):
         }  # Cartesian product of x_dim and y_dim
 
     @beartype
-    def forward(self, x: dict[str, Tensor], z: dict[str, Tensor]) -> dict[str, Tensor]:
+    def forward(self, x: dict[str, Tensor], z: dict[str, Tensor]) -> dict[tuple[str, str], Tensor]:
         """Predict given inputs x and model parameters z.
 
         Args:
@@ -88,10 +88,66 @@ class VanillaPredictor(Predictor):
             z (dict[str, Tensor]): Aggregated context information, each with shape (samples in context , tasks, *).
 
         Returns:
-            dict[str, Tensor]: Predicted outputs, each with shape (samples, tasks, *).
+            dict[tuple[str, str], Tensor]: Predicted values for each tuple (input,output), each with shape (samples, tasks, *).
         """
         y = {
             (input, output): self.predictor[(input, output)](torch.cat([x[input], z[input]], dim=-1))
             for input, output in self.predictor
+        }
+        return y
+
+
+class MetaLinearPredictor(Predictor):
+    @beartype
+    def __init__(
+        self,
+        x_dim: dict[str, int],
+        z_dim: dict[str, int],
+        y_dim: dict[str, int],
+    ) -> None:
+        """
+        This type of predictor computes a linear operation of the latent z and the input x. Here the latent z act as the weights of the linear operation.
+        """
+        super().__init__(x_dim, z_dim, y_dim)
+
+        total_output_dim = sum(y_dim.values())
+        for input_name in z_dim:
+            if z_dim[input_name] / (x_dim[input_name] + 1) != total_output_dim:
+                raise ValueError(
+                    f"dimension mismatch between z_dim and y_dim for {input_name}"
+                )  # check dim as z will be reshaped accordingly
+
+    @beartype
+    def forward(self, x: dict[str, Tensor], z: dict[str, Tensor]) -> dict[tuple[str, str], Tensor]:
+        """Predict given inputs x and model parameters z.
+
+        Args:
+            x (dict[str, Tensor]): Input data, each with shape (samples, tasks, *).
+            z (dict[str, Tensor]): Aggregated context information, each with shape (samples in context , tasks, *).
+
+        Returns:
+            dict[tuple[str, str], Tensor]: Predicted values for each tuple (input,output), each with shape (samples, tasks, *).
+        """
+        # increase feature dim of x for bias
+
+        x = {
+            name: torch.cat([x[name], torch.ones(*x[name].shape[:-1], 1).to(x[name].device)], dim=-1)
+            for name in x
+        }
+        z_resized = (
+            {}
+        )  # (samples, tasks, x_dim + 1, y_dim) for each input/output pair : the last dim of z is reshaped to account for multiple outputs
+        for input in self.z_dim:
+            idx = 0
+            for output in self.y_dim:
+                z_resized[(input, output)] = z[input][
+                    :, :, idx : idx + (self.y_dim[output]) * (self.x_dim[input] + 1)
+                ].reshape(*z[input].shape[:-1], self.x_dim[input] + 1, self.y_dim[output])
+                idx += self.y_dim[output] * (self.x_dim[input] + 1)
+
+        y = {
+            (input, output): torch.einsum("sbi,sbij->sbj", x[input], z_resized[(input, output)])
+            for input in self.x_dim
+            for output in self.y_dim
         }
         return y
