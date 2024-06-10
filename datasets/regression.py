6a000494f369d9614f52458d28ccf9839472be14
sarthmit <sarthmit@gmail.com>
2024-06-10 08:51:38 -0400

    sinusoid + MLP regression

--- datasets/regression.py
+++ datasets/regression.py
@@ -1,13 +1,22 @@
+import copy
 from abc import abstractmethod
 from typing import Any, Iterable, List
 
 import numpy as np
 import torch
+import torch.nn as nn
 from beartype import beartype
+from pytorch_lightning.utilities.seed import isolate_rng
 from torch import FloatTensor, Tensor
 
 from datasets.interfaces import ICLDataModule
 from datasets.synthetic import SyntheticDataset
+
+
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        nn.init.xavier_uniform_(m.weight)
+        nn.init.normal_(m.bias)
 
 
 class RegressionDataset(SyntheticDataset):
@@ -88,7 +97,7 @@ class LinearRegression(RegressionDataset):
     @beartype
     def sample_task_params(self, n: int | None = None) -> dict[str, Tensor]:
         # Linear regression weights
-        n = n if n is not None else self.n_samples
+        n = n if n is not None else self.n_tasks
         w = torch.randn(n, self.x_dim + 1, self.y_dim) / (self.x_dim + 1) ** 0.5
         return {"w": w}
 
@@ -100,3 +109,121 @@ class LinearRegression(RegressionDataset):
         x = torch.cat([x, torch.ones_like(x[:, :, :1])], dim=-1)
         y = torch.bmm(x, w)
         return y
+
+
+class SinusoidRegression(RegressionDataset):
+    @beartype
+    def __init__(
+        self,
+        x_dim: int,
+        y_dim: int,
+        n_tasks: int,
+        n_samples: int,
+        noise: float = 0.0,
+        data_dist: str = "normal",
+        shuffle_samples: bool = True,
+        n_freq: int = 3,
+        fixed_freq: bool = False,
+    ):
+        self.n_freq = n_freq
+        self.fixed_freq = fixed_freq
+        if self.fixed_freq:
+            with isolate_rng():
+                torch.manual_seed(1)
+                self.freqs = torch.rand(x_dim, n_freq).unsqueeze(0) * 5
+
+        super().__init__(
+            x_dim=x_dim,
+            y_dim=y_dim,
+            n_tasks=n_tasks,
+            n_samples=n_samples,
+            noise=noise,
+            data_dist=data_dist,
+            shuffle_samples=shuffle_samples,
+        )
+
+    @beartype
+    def sample_task_params(self, n: int | None = None) -> dict[str, Tensor]:
+        # Linear regression weights
+        n = n if n is not None else self.n_samples
+        amplitudes = torch.rand(n, self.x_dim, self.n_freq)
+        if self.fixed_freq:
+            freq = self.freqs
+            w = amplitudes
+        else:
+            freq = torch.rand(n, self.x_dim, self.n_freq) * 5
+            w = torch.cat([amplitudes, freq], dim=-1)
+        return {"w": w}
+
+    @beartype
+    def function(self, x: Tensor, task_params: dict[str, Tensor]) -> FloatTensor:
+        # x: (bsz, n_samples, x_dim)
+        # w: (bsz, x_dim, 2 * n_freq)
+        w = task_params["w"]
+        if self.fixed_freq:
+            amplitudes = w
+            freq = self.freqs
+        else:
+            amplitudes, freq = w[..., : self.n_freq], w[..., self.n_freq :]
+        x = torch.sin(x.unsqueeze(-1) * freq.unsqueeze(1))
+        y = (x * amplitudes.unsqueeze(1)).sum(dim=-1)
+        return y
+
+
+class MLPRegression(RegressionDataset):
+    @beartype
+    def __init__(
+        self,
+        x_dim: int,
+        y_dim: int,
+        n_tasks: int,
+        n_samples: int,
+        noise: float = 0.0,
+        data_dist: str = "normal",
+        activation: str = "relu",
+        n_layers: int = 1,
+        hidden_dim: int = 64,
+        shuffle_samples: bool = True,
+    ):
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+        if activation == "relu":
+            self.activation = torch.nn.ReLU()
+        elif activation == "tanh":
+            self.activation = torch.nn.Tanh()
+
+        layers = [
+            nn.Linear(x_dim, self.hidden_dim),
+            self.activation,
+        ]
+        for _ in range(self.n_layers - 1):
+            layers.append(nn.Linear(self.hidden_dim, self.hidden_dim))
+            layers.append(self.activation)
+        layers.append(nn.Linear(self.hidden_dim, y_dim))
+        self.model = torch.nn.Sequential(*layers)
+        if torch.cuda.is_available():
+            self.device = torch.device("cuda")
+            self.model = self.model.to(self.device)
+        else:
+            self.device = torch.device("cpu")
+
+        super().__init__(
+            x_dim=x_dim,
+            y_dim=y_dim,
+            n_tasks=n_tasks,
+            n_samples=n_samples,
+            noise=noise,
+            data_dist=data_dist,
+            shuffle_samples=shuffle_samples,
+        )
+
+    def sample_task_params(self, n: int | None = None) -> dict[str, Tensor]:
+        # Linear regression weights
+        models = [copy.deepcopy(self.model).apply(init_weights) for _ in range(n)]
+        return {"w": models}
+
+    @torch.no_grad
+    def function(self, x: Tensor, task_params: dict[str, Tensor]) -> FloatTensor:
+        # x: (bsz, n_samples, x_dim)
+        ys = [task_params["w"][idx](x[idx].to(self.device)) for idx in range(x.shape[0])]
+        return torch.stack(ys)
