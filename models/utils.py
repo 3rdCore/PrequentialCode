6a5e40312942f63ceaa90b606a9430262f137ad3
3rdCore <tom.marty@mila.quebec>
2024-06-18 09:50:49 -0400

    added MLP

--- models/utils.py
+++ models/utils.py
@@ -165,3 +165,17 @@ def fastfood_torched_batched(
     )
 
     return ret
+
+
+class MLP(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
