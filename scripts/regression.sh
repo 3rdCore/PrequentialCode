c4b751edd6e59ea1abb246b722e769f652d77a2f
3rdCore <tom.marty@mila.quebec>
2024-07-03 16:22:13 -0400

    updated bash script

--- scripts/regression.sh
+++ scripts/regression.sh
@@ -1,1 +1,4 @@
-python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/prequential_icl/logs seed=0,1,2,3,4 experiment=regression/${1} ++experiment.task.meta_objective=prequential,train
+python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/prequential_icl/logs seed=0,1,2,3,4 dataset=regression/linear task=meta_optimizer ++logger.tags=[debug] ++task.meta_objective=prequential,train ++task.predictor.n_layers=2 ++datamodule.batch_size=16
+
+
+python train.py --multirun hydra/launcher=mila_tom save_dir=/home/mila/t/tom.marty/scratch/prequential_icl/logs seed=0,1,2,3 dataset=regression/linear task=sgd_optimizer,meta_optimizer ++logger.tags=[debug] ++task.predictor.h_dim=256 ++task.predictor.n_layers=2 ++datamodule.batch_size=16
