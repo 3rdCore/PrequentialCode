dab3f542ca06b8319f2c920bb6568834495e62bd
Eric Elmoznino <eric.elmoznino@gmail.com>
2024-07-09 13:48:12 -0700

    Seed wasn't being logged properly

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ def train(cfg):
     # Add experiment metadata to the logger
     if logger:
         logger.experiment.config.update(OmegaConf.to_container(cfg.experiment, resolve=True))
-        logger.experiment.config.update(OmegaConf.to_container(cfg.seed, resolve=True))
+        logger.experiment.config.update({"seed": cfg.seed})
 
     trainer = Trainer(
         logger=logger,
