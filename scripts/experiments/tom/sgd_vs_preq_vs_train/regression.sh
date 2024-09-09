bfe069dca7edd6c1ba63b1edbdf02a085d44e9c4
3rdCore <tom.marty@mila.quebec>
2024-09-09 15:34:04 -0400

    updated script file

--- scripts/experiments/tom/sgd_vs_preq_vs_train/regression.sh
+++ scripts/experiments/tom/sgd_vs_preq_vs_train/regression.sh
@@ -7,11 +7,12 @@ python train.py --multirun hydra/launcher=mila_tom save_dir=/home/mila/t/tom.mar
 #base-SGD
 python train.py --multirun hydra/launcher=mila_tom save_dir=/home/mila/t/tom.marty/scratch/prequential_icl/logs seed=0,1,2,3,4 dataset=regression/linear,regression/sinusoid,regression/tchebytchev task=sgd_optimizer ++tags=[tom/sgd_vs_preq_vs_train] task/predictor=MLP ++task.predictor.n_layers=2,5 ++task.predictor.h_dim=512 ++datamodule.batch_size=64 ++dataset.train_dataset.n_samples=2000  ++dataset.train_dataset.noise=0.0,0.1 ++task.inner_epochs=50 callbacks=null
 
-#L-reg
-python train.py --multirun hydra/launcher=mila_tom save_dir=/home/mila/t/tom.marty/scratch/prequential_icl/logs seed=0,1,2,3,4 dataset=regression/linear,regression/sinusoid,regression/tchebytchev task=sgd_optimizer ++tags=[tom/sgd_vs_preq_vs_train] task/predictor=MLP ++task.predictor.n_layers=2,5 ++task.predictor.h_dim=512 ++datamodule.batch_size=64 ++dataset.train_dataset.n_samples=2000  ++dataset.train_dataset.noise=0.0,0.1 ++task.regularization_type=L2,L1 ++task.lambda_reg=0.01,0.001,0.0005 ++task.inner_epochs=50 callbacks=null
-
 #Drop-out
-python train.py --multirun hydra/launcher=mila_tom save_dir=/home/mila/t/tom.marty/scratch/prequential_icl/logs seed=0,1,2,3,4 dataset=regression/linear,regression/sinusoid,regression/tchebytchev task=sgd_optimizer ++tags=[tom/sgd_vs_preq_vs_train] task/predictor=MLP ++task.predictor.n_layers=2,5 ++task.predictor.h_dim=512 ++datamodule.batch_size=64 ++dataset.train_dataset.n_samples=2000  ++dataset.train_dataset.noise=0.0,0.1 ++task.predictor.dropout_rate=0.1,0.2,0.3,0.4 ++task.inner_epochs=50 callbacks=null
+python train.py --multirun hydra/launcher=mila_tom save_dir=/home/mila/t/tom.marty/scratch/prequential_icl/logs seed=0,1,2,3,4 dataset=regression/linear,regression/sinusoid,regression/tchebytchev task=sgd_optimizer ++tags=[tom/sgd_vs_preq_vs_train] task/predictor=MLP ++task.predictor.n_layers=2,5 ++task.predictor.h_dim=512 ++datamodule.batch_size=64 ++dataset.train_dataset.n_samples=2000  ++dataset.train_dataset.noise=0.0,0.1 ++task.inner_epochs=50 ++task.predictor.dropout_rate=0.1,0.2,0.3,0.4  callbacks=null
+
+#L-reg
+python train.py --multirun hydra/launcher=mila_tom save_dir=/home/mila/t/tom.marty/scratch/prequential_icl/logs seed=0,1,2 dataset=regression/linear,regression/sinusoid,regression/tchebytchev task=sgd_optimizer ++tags=[tom/sgd_vs_preq_vs_train] task/predictor=MLP ++task.predictor.n_layers=2,5 ++task.predictor.h_dim=512 ++datamodule.batch_size=64 ++dataset.train_dataset.n_samples=2000  ++dataset.train_dataset.noise=0.0,0.1 ++task.regularization_type=L2,L1 ++task.lambda_reg=0.01,0.001 ++task.inner_epochs=50 callbacks=null
+
 
 #Early-Stop
 python train.py --multirun hydra/launcher=mila_tom save_dir=/home/mila/t/tom.marty/scratch/prequential_icl/logs seed=0,1,2,3,4 dataset=regression/linear,regression/sinusoid,regression/tchebytchev task=sgd_optimizer ++tags=[tom/sgd_vs_preq_vs_train] task/predictor=MLP ++task.predictor.n_layers=2,5 ++task.predictor.h_dim=512 ++datamodule.batch_size=64 ++dataset.train_dataset.n_samples=2000  ++dataset.train_dataset.noise=0.0,0.1
