3db4e81f009dbc0b7790555f9a8ae86616ede93a
Eric Elmoznino <eric.elmoznino@gmail.com>
2024-09-09 16:28:08 -0400

    Fixed a typo

--- scripts/experiments/prequential_vs_fixedsize/mastermind.sh
+++ scripts/experiments/prequential_vs_fixedsize/mastermind.sh
@@ -1,7 +1,7 @@
 FIXED_SIZES=(3 9 30)
 
 for FIXED_SIZE in ${FIXED_SIZES[@]}; do
-    python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/prequential_icl/logs task=meta_optimize ++task.meta_objective=prequential ++task.context_aggregator.x_dim=50 ++predictor.x_dim=48 ++predictor.y_dim=18 \
+    python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/prequential_icl/logs task=meta_optimizer_symbolic ++task.meta_objective=prequential ++task.context_aggregator.x_dim=50 ++predictor.x_dim=48 ++predictor.y_dim=18 \
     ++logger.tags=[experiments/prequential_vs_fixedsize/symbolic] \
     seed=0,1,2,3,4 \
     dataset=symbolic/mastermind \
--- scripts/experiments/prequential_vs_fixedsize/sinusoid.sh
+++ scripts/experiments/prequential_vs_fixedsize/sinusoid.sh
@@ -1,7 +1,7 @@
 FIXED_SIZES=(3 9 20)
 
 for FIXED_SIZE in ${FIXED_SIZES[@]}; do
-    python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/prequential_icl/logs task=meta_optimize ++task.meta_objective=prequential \
+    python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/prequential_icl/logs task=meta_optimizer ++task.meta_objective=prequential \
     ++logger.tags=[experiments/prequential_vs_fixedsize/regression] \
     seed=0,1,2,3,4 \
     dataset=regression/sinusoid \
