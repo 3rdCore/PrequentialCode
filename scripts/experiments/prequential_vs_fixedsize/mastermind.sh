6db8b13b94d5b3f63da0d3db9aa75e1459e1100a
Eric Elmoznino <eric.elmoznino@gmail.com>
2024-09-10 20:39:37 -0400

    Changed some mastermind script params

--- scripts/experiments/prequential_vs_fixedsize/mastermind.sh
+++ scripts/experiments/prequential_vs_fixedsize/mastermind.sh
@@ -1,7 +1,7 @@
 FIXED_SIZES=(3 9 30 90 300 900)
 
 for FIXED_SIZE in ${FIXED_SIZES[@]}; do
-    python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/prequential_icl/logs task=meta_optimizer_symbolic ++task.meta_objective=prequential ++task.context_aggregator.x_dim=50 ++predictor.x_dim=48 ++predictor.y_dim=18 \
+    python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/prequential_icl/logs task=meta_optimizer_symbolic ++task.meta_objective=prequential ++task.context_aggregator.x_dim=66 ++predictor.x_dim=48 ++predictor.y_dim=18 ++trainer.max_epochs=200 \
     ++logger.tags=[experiments/prequential_vs_fixedsize/symbolic] \
     seed=0,1,2,3,4 \
     dataset=symbolic/mastermind \
