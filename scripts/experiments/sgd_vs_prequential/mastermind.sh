bc93664ac06bda15a47afe124de5da7b0d20785b
Eric Elmoznino <eric.elmoznino@gmail.com>
2024-09-18 19:28:35 -0400

    Eric mastermind script updated to match Tom's

--- scripts/experiments/sgd_vs_prequential/mastermind.sh
+++ scripts/experiments/sgd_vs_prequential/mastermind.sh
@@ -1,1 +1,18 @@
-python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/prequential_icl/logs seed=0,1,2,3,4 dataset=symbolic/mastermind task=sgd_optimizer ++task.loss_fn._target_=utils.CrossEntropyLossFlat ++predictor.in_features=48 ++predictor.out_features=18 ++predictor.h_dim=256 ++dataset.train_dataset.one_hot_y=False ++dataset.train_dataset.n_samples=3000 ++logger.tags=[experiments/sgd_vs_prequential/symbolic]
+python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/prequential_icl/logs \
+    seed=0,1,2,3,4 \
+    dataset=symbolic/mastermind \
+    task=sgd_optimizer \
+    ++task.loss_fn._target_=utils.CrossEntropyLossFlat \
+    ++predictor.in_features=48 ++predictor.out_features=18 \
+    ++predictor.n_layers=5 ++predictor.h_dim=256 \
+    ++dataset.train_dataset.one_hot_y=False \
+    ++datamodule.batch_size=64 \
+    ++dataset.train_dataset.n_samples=3000 \
+    ++task.inner_epochs=2000 \
+    ++task.lr=0.0001 \
+    ++trainer.max_epochs=2000000 \
+    ++callbacks.monitor=val_loss \
+    ++callbacks.min_delta=1e-3 \
+    ++callbacks.patience=10 \
+    ++trainer.gradient_clip_val=0.05 \
+    ++logger.tags=[experiments/sgd_vs_prequential/symbolic]
