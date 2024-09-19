0847acc69bc0f1e9cc665c86ac032d5ad82c863f
Eric Elmoznino <eric.elmoznino@gmail.com>
2024-09-19 00:50:15 -0400

    More seeds for mastermind sgd

--- scripts/experiments/sgd_vs_prequential/mastermind.sh
+++ scripts/experiments/sgd_vs_prequential/mastermind.sh
@@ -1,5 +1,5 @@
 python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/prequential_icl/logs \
-    seed=0,1,2,3,4 \
+    seed=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14 \
     dataset=symbolic/mastermind \
     task=sgd_optimizer \
     ++task.loss_fn._target_=utils.CrossEntropyLossFlat \
