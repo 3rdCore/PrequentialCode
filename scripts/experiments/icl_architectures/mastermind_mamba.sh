fbe8eb64e5b98af7607c7148a5adae6983a3dda3
Eric Elmoznino <eric.elmoznino@gmail.com>
2024-09-21 13:16:59 -0400

    Updated results notebook with Mamba runs

--- scripts/experiments/icl_architectures/mastermind_mamba.sh
+++ scripts/experiments/icl_architectures/mastermind_mamba.sh
@@ -10,4 +10,4 @@ python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.e
     ++predictor.y_dim=18 \
     ++trainer.max_epochs=80 \
     ++dataset.train_dataset.n_samples=1000 \
-    ++logger.tags=[experiments/icl_architectures/regression]
+    ++logger.tags=[experiments/icl_architectures/symbolic]
