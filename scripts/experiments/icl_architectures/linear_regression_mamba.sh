27d92d7bf86a50dc38935be1a484416c2b382846
EricElmoznino <eric.elmoznino@gmail.com>
2024-09-22 23:23:35 -0400

    Fixed mamba 2 memory issues and gpu compatibilities in run script

--- scripts/experiments/icl_architectures/linear_regression_mamba.sh
+++ scripts/experiments/icl_architectures/linear_regression_mamba.sh
@@ -1,4 +1,8 @@
 MAMBAVERSION=$1
+NSAMPLES=0
+GRES=""
+[ $MAMBAVERSION = 1 ] && NSAMPLES=1000 || NSAMPLES=800
+[ $MAMBAVERSION = 1 ] && GRES="gpu:1" || GRES="gpu:l40s:1"
 
 python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/prequential_icl/logs \
     seed=0,1,2,3,4 \
@@ -9,4 +13,6 @@ python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.e
     ++dataset.train_dataset.noise=0.2 \
     task/context_aggregator=mamba \
     ++task.context_aggregator.mixer_type=Mamba${MAMBAVERSION} \
+    ++dataset.train_dataset.n_samples=${NSAMPLES} \
+    hydra.launcher.gres=${GRES} \
     ++logger.tags=[experiments/icl_architectures/regression]
--- scripts/experiments/icl_architectures/mastermind_mamba.sh
+++ scripts/experiments/icl_architectures/mastermind_mamba.sh
@@ -1,4 +1,8 @@
 MAMBAVERSION=$1
+NSAMPLES=0
+GRES=""
+[ $MAMBAVERSION = 1 ] && NSAMPLES=1000 || NSAMPLES=800
+[ $MAMBAVERSION = 1 ] && GRES="gpu:1" || GRES="gpu:l40s:1"
 
 python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/prequential_icl/logs \
     seed=0,1,2,3,4 \
@@ -12,5 +16,6 @@ python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.e
     ++predictor.x_dim=48 \
     ++predictor.y_dim=18 \
     ++trainer.max_epochs=80 \
-    ++dataset.train_dataset.n_samples=1000 \
+    ++dataset.train_dataset.n_samples=${NSAMPLES} \
+    hydra.launcher.gres=${GRES} \
     ++logger.tags=[experiments/icl_architectures/symbolic]
