acd365acdb093dbab086b1c38dbfef74a9ad493c
Eric Elmoznino <eric.elmoznino@gmail.com>
2024-09-22 20:20:13 -0400

    Updated other mamba scripts to handle mamba2

--- scripts/experiments/icl_architectures/linear_regression_mamba.sh
+++ scripts/experiments/icl_architectures/linear_regression_mamba.sh
@@ -1,3 +1,5 @@
+MAMBAVERSION=$1
+
 python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/prequential_icl/logs \
     seed=0,1,2,3,4 \
     dataset=regression/linear \
@@ -6,4 +8,5 @@ python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.e
     ++dataset.train_dataset.x_dim=3 \
     ++dataset.train_dataset.noise=0.2 \
     task/context_aggregator=mamba \
+    ++context_aggregator.mixer_type=${MAMBAVERSION} \
     ++logger.tags=[experiments/icl_architectures/regression]
--- scripts/experiments/icl_architectures/sinusoid_regression_mamba.sh
+++ scripts/experiments/icl_architectures/sinusoid_regression_mamba.sh
@@ -1,7 +1,10 @@
+MAMBAVERSION=$1
+
 python train.py --multirun hydra/launcher=mila_eric save_dir=/home/mila/e/eric.elmoznino/scratch/prequential_icl/logs \
     seed=0,1,2,3,4 \
     dataset=regression/sinusoid \
     task=meta_optimizer \
     ++task.meta_objective=prequential \
     task/context_aggregator=mamba \
+    ++context_aggregator.mixer_type=${MAMBAVERSION} \
     ++logger.tags=[experiments/icl_architectures/regression]
