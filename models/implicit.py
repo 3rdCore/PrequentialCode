1728f65920fed2dc594b91742e9d71d651528327
Tejas Kasetty <tejaskasetty@gmail.com>
2024-11-13 02:28:16 -0500

    Create a new context aggregator for explicit case
    Transoptimizer2 is a context aggreagator which takes x and y
    as separate tokens instead of single concatenated token.

--- models/implicit.py
+++ models/implicit.py
@@ -93,3 +93,66 @@ class DecoderTransformer(ImplicitModel):
         y = {name: y[i] for i, name in enumerate(self.y_keys)}
 
         return y
+
+
+class DecoderTransformer2(ImplicitModel):
+    @beartype
+    def __init__(
+        self,
+        x_dim: int,  # number of total features in the input
+        y_dim: int,  # number of features in the output
+        h_dim: int,
+        n_layers: int,
+        n_heads: int,
+        x_keys: tuple[str] = ("x",),
+        y_keys: tuple[str] = ("y",),
+        mlp_dim: int | None = None,
+        layer_norm_eps: float = 1e-5,
+        dropout: float = 0.0,
+    ) -> None:
+        super().__init__()
+
+        self.x_dim = x_dim
+        self.y_dim = y_dim
+        self.x_keys = x_keys
+        self.y_keys = y_keys
+
+        self.x_embedding = nn.Linear(x_dim + y_dim, h_dim)
+        self.encoder = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(
+                d_model=h_dim,
+                nhead=n_heads,
+                dim_feedforward=mlp_dim if mlp_dim is not None else 2 * h_dim,
+                dropout=dropout,
+                layer_norm_eps=layer_norm_eps,
+            ),
+            num_layers=n_layers,
+            enable_nested_tensor=False,
+        )
+        self.readout = nn.Linear(h_dim, y_dim)
+
+        self.init_weights()
+
+    @beartype
+    def init_weights(self) -> None:
+        for p in self.encoder.parameters():
+            if p.dim() > 1:  # skip biases
+                nn.init.xavier_uniform_(p)
+
+    @beartype
+    def forward(self, x: dict[str, Tensor]) -> dict[str, Tensor]:
+        # Construct input tensor
+        x = torch.cat([x[name] for name in self.x_keys], dim=-1), torch.cat(
+            [x[name] for name in self.y_keys], dim=-1
+        )
+        seq = self.x_embedding(x)
+        # Encode the sequence
+        causal_mask = torch.nn.Transformer.generate_square_subsequent_mask(seq.shape[0])
+        seq = self.encoder.forward(seq, mask=causal_mask, is_causal=True)
+
+        # Readout
+        y = self.readout(seq)
+        y = y.split(self.y_dim, dim=-1)
+        y = {name: y[i] for i, name in enumerate(self.y_keys)}
+
+        return y
