f9cc2110d8344d2f64f66db6f680f4928970112c
3rdCore <tom.marty@mila.quebec>
2024-11-26 12:47:08 -0500

    added experiment for increasing problem dimension

--- scripts/experiments/tom/paper_experiment.sh
+++ scripts/experiments/tom/paper_experiment.sh
@@ -58,6 +58,19 @@ python train.py --multirun hydra/launcher=mila_tom save_dir=/home/mila/t/tom.mar
 python train.py --multirun hydra/launcher=mila_tom save_dir=/home/mila/t/tom.marty/scratch/prequential_icl/logs seed=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14 dataset=symbolic/mastermind task=sgd_optimizer ++task.loss_fn._target_=utils.CrossEntropyLossFlat ++predictor.in_features=48 ++predictor.out_features=18 ++predictor.n_layers=5 ++predictor.h_dim=256 ++dataset.train_dataset.one_hot_y=False ++logger.tags=[tom/sgd_benchmark,tom/final_benchy] ++datamodule.batch_size=64 ++dataset.train_dataset.n_samples=3000 ++task.inner_epochs=2000 ++task.lr=0.0001 ++trainer.max_epochs=2000000 ++callbacks.monitor=train_loss ++callbacks.min_delta=-1e-4 ++callbacks.patience=50 ++trainer.gradient_clip_val=0.05 ++predictor.dropout_rate=0.2,0.4 &
 
 #########################################################
+#Increasing problem dimension
+
+python train.py --multirun hydra/launcher=mila_tom save_dir=/home/mila/t/tom.marty/scratch/prequential_icl/logs \
+    seed=0,1,2,3,4 \
+    dataset=regression/linear,regression/sinusoid \
+    task=meta_optimizer \
+    ++task.meta_objective=prequential \
+    ++dataset.train_dataset.x_dim=1,2,3,4,5,6,7,8,9,10 \
+    ++dataset.train_dataset.noise=0.2 \
+    ++logger.tags=[experiments/input_dim] \
+    ++tags=[experiments/input_dim]
+
+#########################################################
 #Old Stuff
 #Meta-learner explicit
 #python train.py --multirun hydra/launcher=mila_tom save_dir=/home/mila/t/tom.marty/scratch/prequential_icl/logs seed=0,1,2,3,4 dataset=regression/linear,regression/sinusoid x_dim=1,3 task=meta_optimizer ++tags=[tom/sgd_benchmark] task/predictor=MLPConcatPredictor ++task.meta_objective=prequential,train ++task.predictor.n_layers=5 ++task.predictor.h_dim=512 ++datamodule.batch_size=16 ++dataset.train_dataset.n_samples=2000 ++dataset.train_dataset.noise=0.0,0.2
